0ae3fc5710b5f7148ec6aebc1def9b3f7c3889d5
Carlos Eduardo <duducp2013@gmail.com>
2018-09-24 18:01:12 -0300

    Configuração de Cors

--- controllers/save_response_handler.py
+++ controllers/save_response_handler.py
@@ -1,10 +1,30 @@
 import tornado.web
 import psycopg2
 import datetime
+import json
 
 from config.database import database
 
 class SaveResponseHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        """Set the default response header to be JSON."""
+        self.set_header("Content-Type", 'application/json; charset="utf-8"')
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', '*')
+        self.set_header('Access-Control-Max-Age', 1000)
+        self.set_header('Content-type', 'application/json')
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Allow-Headers',
+                        'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods')
+        self.set_header("X-XSS-Protection", "1; mode=block")
+
+    def OPTIONS(self):
+        pass
+
+    def send_response(self, data, status=200):
+        """Construct and send a JSON response with appropriate status code."""
+        self.set_status(status)
+        self.write(json.dumps(data))
 
     def post(self):
         conn = None
@@ -22,8 +42,7 @@ class SaveResponseHandler(tornado.web.RequestHandler):
             conn.commit()
             cur.close()
 
-            self.set_status(200)
-            self.finish({"message": 'Dado inserido com sucesso!'})
+            self.send_response({"message": 'Dado inserido com sucesso!'})
 
         except (Exception, psycopg2.DatabaseError) as error:
             self.write('Error')

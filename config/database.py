d5f936431fba972e4017492b838dc95c09997750
Carlos Eduardo <duducp2013@gmail.com>
2018-09-24 12:47:21 -0300

    Configuração inicial do projeto

---
+++ config/database.py
@@ -0,0 +1,8 @@
+def database():
+    return {
+        "database": "pg_cedro",
+        "user": "postgres",
+        "password": "edinei6",
+        "host": "localhost",
+        "port": 5432
+    }

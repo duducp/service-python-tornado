a4f91cd4e5838bd92ccd75650d798352a6e6f4ad
Carlos Eduardo <duducp2013@gmail.com>
2018-09-27 13:51:59 -0300

    Adicionado WebSocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import tornado.websocket
 from tornado import web, ioloop, httpserver
 
 from config.create_table import createTables
@@ -9,7 +10,26 @@ from controllers.update_public_sources_query import UpdatePublicSourcesQuery
 from handler.notfound import NotFoundHandler
 
 LISTEN_PORT = int(os.environ.get("PORT", 8000))
-LISTEN_ADDRESS = '127.0.0.1'
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def data_received(self, chunk):
+        pass
+
+    def open(self):
+        print('new connection')
+
+    def on_message(self, message):
+        print('message received:  %s' % message)
+        # Reverse Message and send it back
+        print('sending back message: %s' % message[::-1])
+        self.write_message('uau ' + message[::-1])
+
+    def on_close(self):
+        print('connection closed')
+
+    def check_origin(self, origin):
+        return True
 
 
 def main():
@@ -22,6 +42,7 @@ def main():
 
     # Create tornado application and supply URL routes
     application = web.Application([
+        (r'/ws', WSHandler),
         (r'/', HelloHandler),
         (r'/save', PostPublicSourcesQuery),
         (r'/update/([0-9]+)', UpdatePublicSourcesQuery),
@@ -30,9 +51,9 @@ def main():
 
     # Setup HTTP Server
     http_server = httpserver.HTTPServer(application)
-    http_server.listen(LISTEN_PORT, LISTEN_ADDRESS)
+    http_server.listen(LISTEN_PORT)
 
-    print('Servidor rodando no domínio {}:{}'.format(LISTEN_ADDRESS, LISTEN_PORT))
+    print('Servidor rodando no domínio http://localhost:{}'.format(LISTEN_PORT))
 
     # Start IO/Event loop
     ioloop.IOLoop.instance().start()
